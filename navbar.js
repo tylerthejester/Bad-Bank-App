a4b008ea689dc9866f2d706eb4986ff73976793a
Tam Tran <155765099+tylerthejester@users.noreply.github.com>
2024-06-26 23:46:27 +0700

    update nav bar

--- navbar.js
+++ navbar.js
@@ -9,16 +9,19 @@ function NavBar(){
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+            <a className="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home page">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/deposit/">Deposit</a>
+            <a className="nav-link" href="#/CreateAccount/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Create a new account">Create Account</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+            <a className="nav-link" href="#/deposit/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Deposit to an account">Deposit</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/alldata/">AllData</a>
+            <a className="nav-link" href="#/withdraw/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Withdraw from an account">Withdraw</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/alldata/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="All user data">AllData</a>
           </li>          
         </ul>
       </div>
